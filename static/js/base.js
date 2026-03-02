3eeb4bc997a76bd911d819249db79156f23cc1f6
ayush-trenser <ayush.krishna@trenser.com>
2026-03-02 15:24:15 +0530

    updated error message time

--- static/js/base.js
+++ static/js/base.js
@@ -31,7 +31,7 @@ function showAlert(message, type = 'danger') {
             } else if (wrapper.parentNode) {
                 wrapper.remove();
             }
-        }, 10000);
+        }, 3000);
     } else {
         console.error('alert-container not found in DOM!');
     }
